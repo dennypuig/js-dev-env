7dcc1c51b965faaadce979001e1594418ce4d0a0
Denny Puig Falcon <puig.denny@gmail.com>
2017-07-10 23:19:00 -0400

    lint configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './index.css';
-
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awsome course!`);
+console.log(`This is the value ${courseValue}`); //eslint-disable-line no-console
