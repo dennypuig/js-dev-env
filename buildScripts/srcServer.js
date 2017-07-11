cf901a49267f1b3d6fb2167da70a30d678e9cadb
Denny Puig Falcon <puig.denny@gmail.com>
2017-07-11 01:35:14 -0400

    test enviroment

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  //Hard code for simplicity. Pretend this hit a real database
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob.smith@gmail.com" },
+    { "id": 2, "firstName": "Daniel", "lastName": "Alejandro", "email": "deniel.alejandro@gmail.com" },
+    { "id": 3, "firstName": "Ana", "lastName": "Falcon", "email": "ana.falcon@gmail.com" }
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err); //eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import './index.css';
-import numeral from 'numeral';
+import { getUsers } from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`This is the value ${courseValue}`); //eslint-disable-line no-console
+getUsers().then(result => {
+  let userBody = "";
+
+  result.forEach(user => {
+    userBody += `<tr>
+    <td><a href="#" data-id="${user.Id}" class="deleteUser">Delete</a></td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`;
+  });
+
+  global.document.getElementById('users').innerHTML = userBody;
+});
